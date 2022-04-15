eb3a058c6e5a3c8ddb2a995f0046599544b85370
Denys Kashkovskyi <dannie.k@me.com>
2022-04-15 17:09:27 +0000

    Web: share watch results for clients

--- web/server.go
+++ web/server.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/Kashkovsky/hostmonitor/core"
 	"github.com/gorilla/websocket"
@@ -20,14 +21,18 @@ type Server struct {
 	port     int
 	upgrader *websocket.Upgrader
 	config   *core.WatchConfig
+	results  sync.Map
 }
 
 func NewServer(config *core.WatchConfig, port int) Server {
 	var upgrader = websocket.Upgrader{}
-	return Server{port: port, upgrader: &upgrader, config: config}
+	results := sync.Map{}
+	return Server{port: port, upgrader: &upgrader, config: config, results: results}
 }
 
 func (s *Server) Run() {
+	s.startWatch()
+
 	http.HandleFunc("/ws", s.serveWs)
 	useOS := len(os.Args) > 1 && os.Args[1] == "live"
 	http.Handle("/", http.FileServer(getFileSystem(useOS)))
@@ -40,19 +45,30 @@ func (s *Server) serveWs(w http.ResponseWriter, r *http.Request) {
 		log.Print("upgrade:", err)
 		return
 	}
-
 	defer c.Close()
-	watcher := core.NewWatcher(s.config)
-	mu := sync.Mutex{}
+	s.sendResults(c)
+}
 
-	watcher.Watch(func(res core.TestResult) {
-		mu.Lock()
-		defer mu.Unlock()
-		err = c.WriteJSON(res)
-		if err != nil {
-			log.Println("write:", err)
-		}
+func (s *Server) startWatch() {
+	watcher := core.NewWatcher(s.config)
+	go watcher.Watch(func(res core.TestResult) {
+		s.results.Store(res.Id, res)
 	})
+}
+
+func (s *Server) sendResults(c *websocket.Conn) {
+	for {
+		s.results.Range(func(_ any, res interface{}) bool {
+			err := c.WriteJSON(res)
+			if err != nil {
+				log.Println("write:", err)
+				return false
+			}
+			return true
+		})
+
+		time.Sleep(time.Second)
+	}
 }
 
 func getFileSystem(useOS bool) http.FileSystem {
